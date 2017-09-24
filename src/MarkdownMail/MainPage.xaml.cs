2d58748f144ff77b06af83d962ec3161a9e68a21
walterlv <xblero@gmail.com>
2017-09-24 10:27:14 +0800

    Add initial project files.

---
+++ src/MarkdownMail/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Walterlv.MarkdownMail
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/MarkdownMail/Pages/WritingPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Walterlv.MarkdownMail
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class WritingPage : Page
+    {
+        public WritingPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
