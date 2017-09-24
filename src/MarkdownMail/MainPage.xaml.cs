b4d94596c4e29a1557034669efbe09cf85de741c
walterlv <xblero@gmail.com>
2017-09-24 11:32:59 +0800

    Add receiving button and imap receiving function.

--- src/MarkdownMail/MainPage.xaml.cs
+++ src/MarkdownMail/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MailKit;
+using MailKit.Net.Imap;
 
 namespace Walterlv.MarkdownMail
 {
@@ -9,7 +14,30 @@ namespace Walterlv.MarkdownMail
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+        }
+
+        private async void ReceiveButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new ImapClient())
+            {
+                client.Connect("imap.cvte.com", 993, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                client.Authenticate(UserNameBox.Text, PasswordBox.Password);
+
+                var inbox = client.Inbox;
+                inbox.Open(FolderAccess.ReadOnly);
+
+                TestTextBlock.Text += $"Total messages: {inbox.Count}{Environment.NewLine}";
+                TestTextBlock.Text += $"Recent messages: {inbox.Recent}{Environment.NewLine}";
+
+                for (var i = 0; i < inbox.Count; i++)
+                {
+                    await Task.Delay(100);
+                    var message = inbox.GetMessage(i);
+                    TestTextBlock.Text += $"Subject: {message.Subject}{Environment.NewLine}";
+                }
+            }
         }
     }
 }
